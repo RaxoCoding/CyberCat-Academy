e091a31a78ba2d1e480682fa49a1c7b1a21b573e
Gomez0015 <oscargomez.raxo@gmail.com>
2024-09-18 10:08:05 +0200

    styling(scoreboard-skeleton): Remove padding for scoreboard skeleting as it is handled by layout.

--- src/app/scoreboard/loading.tsx
+++ src/app/scoreboard/loading.tsx
@@ -1,37 +1,54 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { Skeleton } from "@/components/ui/skeleton"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ScoreboardPageSkeleton() {
   return (
-    <div className="container mx-auto py-8">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl font-bold text-center">
-            <Skeleton className="h-8 w-3/4 mx-auto" />
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]"><Skeleton className="h-4 w-full" /></TableHead>
-                <TableHead><Skeleton className="h-4 w-full" /></TableHead>
-                <TableHead className="text-right"><Skeleton className="h-4 w-full" /></TableHead>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold text-center">
+          <Skeleton className="h-8 w-3/4 mx-auto" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">
+                <Skeleton className="h-4 w-full" />
+              </TableHead>
+              <TableHead>
+                <Skeleton className="h-4 w-full" />
+              </TableHead>
+              <TableHead className="text-right">
+                <Skeleton className="h-4 w-full" />
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {[...Array(10)].map((_, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">
+                  <Skeleton className="h-4 w-8" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-full" />
+                </TableCell>
+                <TableCell className="text-right">
+                  <Skeleton className="h-4 w-16 ml-auto" />
+                </TableCell>
               </TableRow>
-            </TableHeader>
-            <TableBody>
-              {[...Array(10)].map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell className="font-medium"><Skeleton className="h-4 w-8" /></TableCell>
-                  <TableCell><Skeleton className="h-4 w-full" /></TableCell>
-                  <TableCell className="text-right"><Skeleton className="h-4 w-16 ml-auto" /></TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    </div>
-  )
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
 }
