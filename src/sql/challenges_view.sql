70449a3b8f72333ee911cce881227f78a25e0b22
Fanta <hugoduche37@gmail.com>
2024-09-26 16:02:52 +0200

    fix: remove previously added 's' in challenges_view.sql

--- src/sql/challenges_view.sql
+++ src/sql/challenges_view.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS public_challenges;
 
 CREATE VIEW public_challenges AS
-SELECT id, name, description, points, created_at, category_id, tags, url, writeup, ressources, files, author_id
+SELECT id, name, description, points, created_at, category_id, tags, url, writeups, ressources, files, author_id
 FROM challenges;
 
 GRANT SELECT ON public_challenges TO public;
